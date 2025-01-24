9446423bede8f1b47534bfaa1fac42c60ae098d9
vikrant235 <veersinghvikrant51@gmail.com>
2025-01-24 12:05:52 +0530

    commit_app.py_1

---
+++ app.py
@@ -0,0 +1,11 @@
+print('unit converter')
+input_weight=int(input("write your weight"))
+kg_or_lb=input('(K)kg or (l)lb')
+kg_or_lb.lower()
+if kg_or_lb == "k":
+    print(input_weight*2.205)
+elif kg_or_lb == "l":
+    print(int(input_weight / 2.205))
+else :
+    print("invalid input")
+print("done")
